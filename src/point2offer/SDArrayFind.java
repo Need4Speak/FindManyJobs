8334e9f351cbaa38fc077ae6cbdef1d4b7ad9180
I353561 <I353561@CTUN56600647A.apj.global.corp.sap>
2018-08-06 13:50:53 +0800

    Run 2d array search successfully

--- src/point2offer/SDArrayFind.java
+++ src/point2offer/SDArrayFind.java
@@ -5,47 +5,61 @@ public class SDArrayFind {
     public static void main(String[] args) {
         int[][]  arr = {{1,2,3},{4,5,6},{7,8,9}};
         int[][] arr2 = {{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
-        for (int indexRow = 0; indexRow < arr.length;indexRow++) {
-            for (int indexColumn = 0; indexColumn < arr[0].length;indexColumn++) {
-                System.out.print(arr[indexRow][indexColumn]);
+        int[][]  arr3 = {{1,2},{4,5}};
+        for (int indexRow = 0; indexRow < arr2.length;indexRow++) {
+            for (int indexColumn = 0; indexColumn < arr2[0].length;indexColumn++) {
+                System.out.print(arr2[indexRow][indexColumn]);
             }
             System.out.println();
         }
 
-        System.out.println(Find(7, arr2));
+        System.out.println(Find(66, arr2));
 
 
     }
 
     public static  boolean Find(int target, int[][] array) {
+    	if(array.length == 0 || array[0].length == 0)
+    		return false;
         int rowLength = array.length;
         int columnLength = array[0].length;
 
         int x = 0;
         int y = columnLength - 1;
-        while(x < rowLength && y >= 0) {
+        
+        int index;
+        while(!(x == rowLength - 1 && y == 0)) {
             if (target == array[x][y]) {
                 return true;
             } else if (target > array[x][y]) {
-                for (int index = x; index < rowLength; index++) {
+                for (index = x; index < rowLength; index++) {
                     if (target == array[index][y]) {
                         return true;
                     } else if (target < array[index][y]) {
                         x = index;
                         break;
                     }
+                    
+                    if(index == rowLength - 1 && target > array[index][y]) {
+                    	//x = rowLength - 1;
+                    	return false;
+                    }
                 }
-                return false;
+                //return false;
             } else if (target < array[x][y]) {
-                for (int index = y; index >= 0; index--) {
+                for (index = y; index >= 0; index--) {
                     if (target == array[x][index]) {
                         return true;
                     } else if (target > array[x][index]) {
                         y = index;
                         break;
                     }
+                    
+                    if(index == 0 && target < array[x][index]) {
+                    	return false;
+                    }
                 }
-                return false;
+                //return false;
             }
         }
      return false;
