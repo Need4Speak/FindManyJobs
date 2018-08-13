e81c5c4128a2f26b26140471411dde63b69f855e
chao <zcsd2668@163.com>
2018-08-13 23:23:59 +0800

    先序中序生成二叉树

---
+++ src/point2offer/ReConstructBinaryTree_4.java
@@ -0,0 +1,55 @@
+package point2offer;
+
+import java.util.Arrays;
+
+/**
+ * Definition for binary tree
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class ReConstructBinaryTree_4 {
+    private static ReConstructBinaryTree_4 bt = new ReConstructBinaryTree_4();
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        if (pre.length == 0 || in.length == 0)
+            return null;
+        else {
+            TreeNode treeNode = new TreeNode(pre[0]);
+            int i = 0;
+            while (i < in.length) {
+                if (in[i] == pre[0]) {
+                    break;
+                }
+                i++;
+            }
+            treeNode.left = reConstructBinaryTree(Arrays.copyOfRange(pre, 1, i+1),
+                    Arrays.copyOfRange(in, 0, i));
+            treeNode.right = reConstructBinaryTree(Arrays.copyOfRange(pre, i+ 1, pre.length),
+                    Arrays.copyOfRange(in, i + 1, in.length));
+            return treeNode;
+        }
+
+    }
+
+    public void inTraverse(TreeNode treeNode) {
+        if (treeNode!=null) {
+            inTraverse(treeNode.left);
+            System.out.println(treeNode.val);
+            inTraverse(treeNode.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] pre = {1, 2, 4, 7, 3, 5, 6, 8};
+        int[] in = {4, 7, 2, 1, 5, 3, 8, 6};
+        TreeNode root = bt.reConstructBinaryTree(pre, in);
+        bt.inTraverse(root);
+    }
+}
