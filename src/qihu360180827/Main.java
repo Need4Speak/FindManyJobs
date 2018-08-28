b3aea6fd38cd81788941bc2ca1cbfd108c265452
I353561 <I353561@CTUN56600647A.apj.global.corp.sap>
2018-08-28 10:56:37 +0800

    360笔试0828

---
+++ src/qihu360180827/Main.java
@@ -0,0 +1,48 @@
+package qihu360180827;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int peoples = Integer.parseInt(in.nextLine());
+		if (peoples < 2 || peoples > 1000) {
+			System.out.println(0);
+		}
+		else {
+			int[] x = new int[peoples];
+			int[] y = new int[peoples];
+			for (int i = 0; i < peoples; i++) {
+				String input = in.nextLine();
+				x[i] = Integer.parseInt(input.split(" ")[0]);
+				y[i] = Integer.parseInt(input.split(" ")[1]);
+			}
+			int maxX = x[0];
+			int maxY = y[0];
+			int minX = x[0];
+			int minY = y[0];
+			for (int i = 1; i < peoples; i++) {
+				if (x[i] > maxX) {
+					maxX = x[i];
+				} 
+				if(x[i] < minX) {
+					minX = x[i];
+				}
+				if (y[i] > maxY) {
+					maxY = y[i];
+				}
+				if(y[i] < minY) {
+					minY = y[i];
+				}
+			}
+			int width = maxX - minX;
+			int height = maxY - minY;
+			if (width  > height) {
+				System.out.println(width * width);
+			} else {
+				System.out.println(height * height);
+			}
+		}
+
+	}
+}
---
+++ src/qihu360180827/Main2.java
@@ -0,0 +1,38 @@
+package qihu360180827;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main2 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String firstLine = in.nextLine();
+		int flowers = Integer.parseInt(firstLine.split(" ")[0]);
+		int lookCounts = Integer.parseInt(firstLine.split(" ")[1]);
+		// a[i]表示第i次，小明看的花的种类;
+		String[] a = in.nextLine().split(" ");
+		int problem = Integer.parseInt(in.nextLine());
+		if (0 == flowers || 0 == lookCounts || 0 == problem) {
+			System.out.println(0);
+		} else {
+			//String[] area = new String[problem];
+			List<String> area = new ArrayList<String>();
+			for (int i = 0; i < problem; i++) {
+				area.add(in.nextLine());
+			}
+			Set<Integer> set = new HashSet<Integer>();
+			for (int i = 0; i < problem; i++) {
+				int start = Integer.parseInt(area.get(i).split(" ")[0]);
+				int end = Integer.parseInt(area.get(i).split(" ")[1]);
+				set.clear();
+				for (int j = start - 1; j < end; j++) {
+					set.add(Integer.parseInt(a[j]));
+				}
+				System.out.println(set.size());
+			}
+		}
+	}
+}
